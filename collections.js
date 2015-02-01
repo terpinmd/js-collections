582086503f6e4e4d5b2859a0821101a86bc59b1d
Jeremy Hammond <jeremy.hammond@nextcentury.com>
2015-02-01 11:01:01 -0500

    seeding tests

--- collections.js
+++ collections.js
@@ -1,73 +1,92 @@
-(function () {
-    
-    var Collections =  window.Collections = function(){
-      if (obj instanceof Collections) return obj;
-      if (!(this instanceof Collections)) return new Collections(obj);
-      this._wrapped = obj;   
-    };
-    
+(function(exports){
+  var _Collections = function(){
+    this.version = 1.0;
+  };
 
-    var _list = function(){
-      var elements = [];
-      var me = this;
 
-      var iterate = function(items, func){
-        for(var i = 0; i < items.length ; i++){
-          var result = func.call(me, items[i], i);
-          if(result === true){
-            break;
-          }
-        }  
-      };
+  _Collections.prototype.list = function(){
+      var _list = function(){
+        var elements = [];
+        var me = this;
 
-      this.add = function(item){
-        elements.push(item);
-      }
-      
-      this.addAll = function(collection){
-        if(collection instanceof _list){
-          console.log("yooooooooooooooooooooooooo");
-        } else{
-          console.log("noo");
+        var iterate = function(items, func){
+          for(var i = 0; i < items.length ; i++){
+            var result = func.call(me, items[i], i);
+            if(result === true){
+              break;
+            }
+          }  
+        };
+
+        this.add = function(item){
+          elements.push(item);
         }
-      };
-
-      this.clear = function(){
-        elements = null;
-        elements = [];
-      };
-
-      this.isEmpty = function(){
-        return this.elements.length === 0;
-      };
-
-      this.contains = function(item){
-        var found = false;
-        iterate(elements, function(value, idx){
-          var el = elements[idx];
-          if(el === item){
-            found = true;
-            return true;
+        
+        this.addAll = function(collection){
+          if(collection instanceof _list){
+            console.log("yooooooooooooooooooooooooo");
           } else{
-            if(typeof item === "function"){
-              found = item(el);
-              if(found) return true;
-            } 
+            console.log("noo");
           }
-        });
-        return found;
-      };
+        };
 
-      this.size = function(){
-        return elements.length;
-      };
-    };
- 
+        this.clear = function(){
+          elements = null;
+          elements = [];
+        };
 
-    Collections.list = function(){
-      return new _list();
+        this.isEmpty = function(){
+          return this.elements.length === 0;
+        };
+
+        this.contains = function(item){
+          var found = false;
+          iterate(elements, function(value, idx){
+            var el = elements[idx];
+            if(el === item){
+              found = true;
+              return true;
+            } else{
+              if(typeof item === "function"){
+                found = item(el);
+                if(found) return true;
+              } 
+            }
+          });
+          return found;
+        };
+
+        this.size = function(){
+          return elements.length;
+        };
+      };
+    return new _list();
+  }
+
+  exports.Collections =  new _Collections();
+  
+})(this);
+
+
+
+/*
+
+// cow.js
+(function(exports) {
+  "use strict";
+
+  function Cow(name) {
+    this.name = name || "Anon cow";
+  }
+  exports.Cow = Cow;
+
+  Cow.prototype = {
+    greets: function(target) {
+      if (!target)
+        throw new Error("missing target");
+      return this.name + " greets " + target;
     }
+  };
+})(this);
 
-    
-    return Collections;
-}());
+*/
